f9c0ddb76a4f47176a058983cba45a4ecf6ae8b2
Christian Medinilla <cris.medinill@gmail.com>
2023-09-21 18:16:03 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * even or odd <https://github.com/cmedinilla/even-or-odd>
+ *
+ * Copyright (c) 2023, Christian Medinilla
+ * Released under the MIT License.
+ */
+
+'use strict';
+
+const isOdd = require('is-odd');
+const isEven = require('is-even');
+
+const evenOrOdd = () => {
+
+}
+
+module.exports = { isEven, isOdd, evenOrOdd }
